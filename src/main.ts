3fee771aa60ae1fd83827beffd4f42218d85c555
Omar Pava <omarpava@MacBook-Pro-de-Omar.local>
2025-04-23 20:37:49 -0500

    feat: ajuste para valida variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000);
+  console.log('🧪 ENV', {
+    DB_HOST: process.env.DB_HOST,
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+  });
 }
 bootstrap();
